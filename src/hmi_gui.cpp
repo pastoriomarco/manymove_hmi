4bdf0df8f26cbe07fea9b65655017ed6365affb5
pastoriomarco <pastoriomarco@gmail.com>
2025-02-02 22:04:35 +0100

    initial draft of HMI buttons

--- src/hmi_gui.cpp
+++ src/hmi_gui.cpp
@@ -13,8 +13,13 @@ HmiGui::HmiGui(QWidget *parent)
 
     // Create the three buttons.
     startButton_ = new QPushButton("START", this);
-    stopButton_  = new QPushButton("STOP", this);
+    stopButton_ = new QPushButton("STOP", this);
     resetButton_ = new QPushButton("RESET", this);
+
+    // Set object names so that the style sheet rules for #startButton, etc., apply.
+    startButton_->setObjectName("startButton");
+    stopButton_->setObjectName("stopButton");
+    resetButton_->setObjectName("resetButton");
 
     // Set initial states (assume that with stop_execution true, START and RESET are enabled).
     startButton_->setEnabled(true);
@@ -23,7 +28,7 @@ HmiGui::HmiGui(QWidget *parent)
 
     // Connect the button signals to our internal slots.
     connect(startButton_, &QPushButton::clicked, this, &HmiGui::onStartClicked);
-    connect(stopButton_,  &QPushButton::clicked, this, &HmiGui::onStopClicked);
+    connect(stopButton_, &QPushButton::clicked, this, &HmiGui::onStopClicked);
     connect(resetButton_, &QPushButton::clicked, this, &HmiGui::onResetClicked);
 
     // Layout the buttons.
@@ -36,28 +41,37 @@ HmiGui::HmiGui(QWidget *parent)
     // Set up a TCP server listening on port 5000.
     tcpServer_ = new QTcpServer(this);
     connect(tcpServer_, &QTcpServer::newConnection, this, &HmiGui::onNewConnection);
-    if (!tcpServer_->listen(QHostAddress::Any, 5000)) {
+    if (!tcpServer_->listen(QHostAddress::Any, 5000))
+    {
         qWarning() << "TCP Server could not start!";
-    } else {
+    }
+    else
+    {
         qDebug() << "TCP Server listening on port 5000";
     }
 }
 
-HmiGui::~HmiGui() {
-    if (clientSocket_) {
+HmiGui::~HmiGui()
+{
+    if (clientSocket_)
+    {
         clientSocket_->disconnectFromHost();
     }
 }
 
-void HmiGui::updateStatus(bool execution_resumed, bool stop_execution, bool abort_mission) {
+void HmiGui::updateStatus(bool execution_resumed, bool stop_execution, bool abort_mission)
+{
     // Update button states:
     // - STOP button is enabled only if stop_execution is false.
     // - START and RESET are enabled only if stop_execution is true.
-    if (stop_execution) {
+    if (stop_execution)
+    {
         startButton_->setEnabled(true);
         resetButton_->setEnabled(true);
         stopButton_->setEnabled(false);
-    } else {
+    }
+    else
+    {
         startButton_->setEnabled(false);
         resetButton_->setEnabled(false);
         stopButton_->setEnabled(true);
@@ -65,41 +79,48 @@ void HmiGui::updateStatus(bool execution_resumed, bool stop_execution, bool abor
 
     // Build a JSON string representing the current status.
     lastStatusJson_ = QString("{\"execution_resumed\": %1, \"stop_execution\": %2, \"abort_mission\": %3}")
-                      .arg(execution_resumed ? "true" : "false")
-                      .arg(stop_execution ? "true" : "false")
-                      .arg(abort_mission ? "true" : "false");
+                          .arg(execution_resumed ? "true" : "false")
+                          .arg(stop_execution ? "true" : "false")
+                          .arg(abort_mission ? "true" : "false");
 
     // If a TCP client is connected, send the updated status.
-    if (clientSocket_ && clientSocket_->state() == QAbstractSocket::ConnectedState) {
+    if (clientSocket_ && clientSocket_->state() == QAbstractSocket::ConnectedState)
+    {
         clientSocket_->write(lastStatusJson_.toUtf8());
         clientSocket_->flush();
     }
 }
 
-void HmiGui::onStartClicked() {
+void HmiGui::onStartClicked()
+{
     emit startExecutionRequested();
 }
 
-void HmiGui::onStopClicked() {
+void HmiGui::onStopClicked()
+{
     emit stopExecutionRequested();
 }
 
-void HmiGui::onResetClicked() {
+void HmiGui::onResetClicked()
+{
     emit resetProgramRequested();
 }
 
-void HmiGui::onNewConnection() {
+void HmiGui::onNewConnection()
+{
     clientSocket_ = tcpServer_->nextPendingConnection();
     connect(clientSocket_, &QTcpSocket::disconnected, this, &HmiGui::onSocketDisconnected);
     qDebug() << "New TCP client connected.";
     // Optionally, send the current status immediately.
-    if (!lastStatusJson_.isEmpty()) {
+    if (!lastStatusJson_.isEmpty())
+    {
         clientSocket_->write(lastStatusJson_.toUtf8());
         clientSocket_->flush();
     }
 }
 
-void HmiGui::onSocketDisconnected() {
+void HmiGui::onSocketDisconnected()
+{
     qDebug() << "TCP client disconnected.";
     clientSocket_ = nullptr;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,65 @@
 #include <thread>
 #include <memory>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Initialize ROS2.
     rclcpp::init(argc, argv);
 
     // Initialize the Qt application.
     QApplication app(argc, argv);
+
+    // Set a global style sheet.
+    app.setStyleSheet(
+        // Global style for all QPushButtons
+        "QPushButton { "
+        "  font: bold 16px; "
+        "  padding: 10px; "
+        "  min-width: 120px; "
+        "  min-height: 50px; "
+        "} "
+
+        // Global disabled style for all QPushButtons
+        "QPushButton:disabled { "
+        "  background-color: grey; "
+        "  color: darkgrey; "
+        "} "
+
+        // Specific style for the start button when enabled.
+        "QPushButton#startButton { "
+        "  background-color: green; "
+        "  color: black; "
+        "} "
+
+        // Specific disabled style for the start button.
+        "QPushButton#startButton:disabled { "
+        "  background-color: grey; "
+        "  color: darkgrey; "
+        "} "
+
+        // Specific style for the stop button when enabled.
+        "QPushButton#stopButton { "
+        "  background-color: red; "
+        "  color: black; "
+        "} "
+
+        // Specific disabled style for the stop button.
+        "QPushButton#stopButton:disabled { "
+        "  background-color: grey; "
+        "  color: darkgrey; "
+        "} "
+
+        // Specific style for the reset button when enabled.
+        "QPushButton#resetButton { "
+        "  background-color: yellow; "
+        "  color: black; "
+        "} "
+
+        // Specific disabled style for the reset button.
+        "QPushButton#resetButton:disabled { "
+        "  background-color: grey; "
+        "  color: darkgrey; "
+        "} ");
 
     // Create the HMI GUI window.
     HmiGui gui;
@@ -19,28 +72,25 @@ int main(int argc, char *argv[]) {
     // Create the ROS2 worker node and pass the pointer to the GUI.
     auto ros2_worker = std::make_shared<Ros2Worker>("hmi_ros2_worker", &gui);
 
-    // Connect GUI button signals to the corresponding service calls in the ROS2 node.
-    QObject::connect(&gui, &HmiGui::startExecutionRequested, [ros2_worker]() {
-        ros2_worker->callStartExecution();
-    });
-    QObject::connect(&gui, &HmiGui::stopExecutionRequested, [ros2_worker]() {
-        ros2_worker->callStopExecution();
-    });
-    QObject::connect(&gui, &HmiGui::resetProgramRequested, [ros2_worker]() {
-        ros2_worker->callResetProgram();
-    });
+    // Connect GUI button signals to the corresponding service calls.
+    QObject::connect(&gui, &HmiGui::startExecutionRequested, [ros2_worker]()
+                     { ros2_worker->callStartExecution(); });
+    QObject::connect(&gui, &HmiGui::stopExecutionRequested, [ros2_worker]()
+                     { ros2_worker->callStopExecution(); });
+    QObject::connect(&gui, &HmiGui::resetProgramRequested, [ros2_worker]()
+                     { ros2_worker->callResetProgram(); });
 
     // Run the ROS2 event loop in a separate thread.
-    std::thread ros2_thread([ros2_worker]() {
-        rclcpp::spin(ros2_worker);
-    });
+    std::thread ros2_thread([ros2_worker]()
+                            { rclcpp::spin(ros2_worker); });
 
     // Run the Qt main loop.
     int ret = app.exec();
 
     // On exit, shutdown ROS2 and join the ROS2 thread.
     rclcpp::shutdown();
-    if (ros2_thread.joinable()) {
+    if (ros2_thread.joinable())
+    {
         ros2_thread.join();
     }
     return ret;
